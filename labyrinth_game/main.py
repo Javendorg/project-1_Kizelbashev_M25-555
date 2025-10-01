91a1e9095424ea7d729bb77a0e129a1bb1823546
javendorg <javendorg777@yandex.ru>
2025-10-01 20:56:44 +0300

    2.1

--- labyrinth_game/main.py
+++ labyrinth_game/main.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python3
-
+from . import constants
+from . import player_actions
+from . import utils
 
 def main():
     print("Первая попытка запустить проект!")
 
 if __name__ == "__main__":
     main()
+
+
+game_state = {
+    'player_inventory': [], # Инвентарь игрока
+    'current_room': 'entrance', # Текущая комната
+    'game_over': False, # Значения окончания игры
+    'steps_taken': 0 # Количество шагов
+}
